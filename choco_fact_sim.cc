ff9d4a98bff6f7a3bad39e0438550f41616b896d
David Kašpar <deekej.dj@gmail.com>
2013-12-08 12:18:04 +0100

    Constants & data types added. Facilities, processes & events added.

--- choco_fact_sim.cc
+++ choco_fact_sim.cc
@@ -22,6 +22,10 @@
  *                uses SimLib/C++ simulation library (version 3), which
  *                is distributed by GPLv1 license.
  *
+ * NOTE:          This source file is written in English, because we both
+ *                agreed it's common standard and we don't like source files
+ *                using Czech.
+ *
  * More info @:   http://perchta.fit.vutbr.cz:8000/vyuka-ims/31
  *
  * File encoding: en_US.utf8 (United States)
@@ -44,6 +48,7 @@
 #include <string>
 
 /* C header files. */
+#include <cmath>
 #include <cstdlib>
 #include <cstring>
 
@@ -52,21 +57,204 @@
 
 
 /* *********************************************************************************************************************************************************** *
- ~ ~~~[ CONSTANTS ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~
+ ~ ~~~[ CONSTANTS & DATA TYPES DECLARATIONS ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~
  * *********************************************************************************************************************************************************** */
 
+enum chocolate_type {
+  WHITE = 0,
+  MILK,
+  DARK,
+};
+
+
+enum order_chances_types {
+  TAKING_REST = 0,
+  TRYING_COMPETITION,
+  TRYING_COMPETITION2,
+};
+
+
+enum store_occupancy_types {
+  MIN_OCCUPANCY = 0,
+  ADEQUATE_OCCUPANCY,
+};
+
+
+double order_chances[][4] = {
+  {31, 48, 27},
+  {28, 54, 33},
+  {30, 51, 29},
+};
+
+
+double store_occupancy[][2] = {
+  {20, 70},
+  {25, 80},
+  {20, 30},
+};
+
+
+static const double NEW_ORDER_WHITE_CHANCE = 13;
+static const double NEW_ORDER_MILK_CHANCE = NEW_ORDER_WHITE_CHANCE + 49;
 
 
 /* *********************************************************************************************************************************************************** *
- ~ ~~~[ DATA TYPES DECLARATIONS ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~
+ ~ ~~~[ FACILITIES ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~
  * *********************************************************************************************************************************************************** */
 
+class storehouse : public Facility {
+  private:
+    unsigned max_capacity;
+    unsigned act_capacity;
+    enum chocolate_type store_type;
+
+  public:
+
+#if 0
+  storehouse(enum chocolate_type type, unsigned max_cap, unsigned init_cap) : Facility() {
+    this->store_type = type;
+    this->max_capacity = max_cap;
+    this->act_capacity = init_cap;
+  }
+#endif
+
+  storehouse(const char *name, enum chocolate_type type, unsigned max_cap, unsigned init_cap) : Facility(name) {
+    this->store_type = type;
+    this->max_capacity = max_cap;
+    this->act_capacity = init_cap;
+  }
+
+#if 0
+  storehouse(const char *name, Queue *q, enum chocolate_type type, unsigned max_cap, unsigned init_cap) : Facility(name, q) {
+    this->store_type = type;
+    this->max_capacity = max_cap;
+    this->act_capacity = init_cap;
+  }
+#endif
+
+  int withdraw(unsigned amount) {
+    if (amount > this->max_capacity) {
+      std::cerr << "SIMULATION ERROR: Order is not allowed to withdraw bigger from storehouse" << std::endl;
+      std::cerr << "                  than is current maximum capacity!" << std::endl;
+      std::cerr << "                  Please, FIX your simulation PARAMETERS." << std::endl;
+
+      exit(EXIT_FAILURE);
+    }
+    else if (amount > this->act_capacity) {
+      return EXIT_FAILURE;            /* This amount can't be currently withdraw. */
+    }
+
+    this->act_capacity -= amount;
+  
+    double percentage = static_cast<double>(this->act_capacity) / static_cast<double>(this->max_capacity) * 100;
+
+    if (percentage < store_occupancy[this->store_type][MIN_OCCUPANCY]) {
+    }
+
+    if (percentage < store_occupancy[this->store_type][ADEQUATE_OCCUPANCY]) {
+    }
+
+    // TODO:
+  }
+};
+
+
+/* 
+ *Global instances of chocolate stores. 
+ */
+storehouse white_store("White chocolate store", WHITE, 750, 0);
+storehouse milk_store(" Milk chocolate store", MILK, 2000, 0);
+storehouse dark_store(" Dark chocolate store", DARK, 1500, 0);
+
+
+/*
+ * Global instances of other facilities, used during new batch process.
+ */
+Facility cleaning("Cleaning of chocolate beans");
+Facility roasting("Roasting of chocolate beans");
+Facility beans_grinding("Grinding of cocoa nibs or chocolate paste");
+Facility refining("Refining of cocoa brash/chocolate paste");
+Facility defatting("Defatting of cocoa liquor");
+Facility cake_grinding("Grinding of cocoa press cake");
+Facility remixing("Chocolate paste remixing");
+Facility conching("Chocolate paste conching");
+Facility tempering("Chocolate tempering");
+Facility molding("Chocolate molding into blocks");
+Facility packing("Chocolate blocks packing");
+
+
+Facility shell_removing("Removing cocoa beans' shells");
+
+/* *********************************************************************************************************************************************************** *
+ ~ ~~~[ PROCESSES ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~
+ * *********************************************************************************************************************************************************** */
+
+/**
+ * Simulation process for new order in the system.
+ */
+class order : public Process {
+  private:
+    unsigned order_size;
+    enum chocolate_type order_type;
+  
+  public:
+    /*
+     * Constructor.
+     */
+    order(enum chocolate_type type) : Process() {
+      this->order_type = type;
+
+      /* Size from 1 to 100 inclusive. (static_cast rounds downward) */
+      this->order_size = static_cast<unsigned>(Uniform(1,101));
+    }
+
+    void Behavior(void) {
+
+    }
+};
+
+
+/**
+ * Simulation process for new chocolate batch.
+ */
+class batch : public Process {
+  private:
+    unsigned quantity;
+    enum chocolate_type batch_type;
+
+  public:
+  
+    void Behavior(void) {
+
+    }
+};
 
 
 /* *********************************************************************************************************************************************************** *
- ~ ~~~[ PRIMARY FUNCTIONS ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~
+ ~ ~~~[ EVENTS ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~
  * *********************************************************************************************************************************************************** */
 
+/**
+ * New orders generator for the chocolate factory system.
+ */
+class generator : public Event {
+  void Behavior(void) {
+
+    double roll = Uniform(0,100);
+
+    if (roll <= NEW_ORDER_WHITE_CHANCE) {
+      (new order(WHITE))->Activate();
+    }
+    else if (roll <= NEW_ORDER_MILK_CHANCE) {
+      (new order(MILK))->Activate();
+    }
+    else {
+      (new order(DARK))->Activate();
+    }
+
+    this->Activate(Time + Exponential(90));
+  }
+};
 
 
 /* *********************************************************************************************************************************************************** *
@@ -120,7 +308,7 @@ int main(int argc, char* argv[])
   seed.read(reinterpret_cast<char *>(&seed_value), sizeof(long));
   RandomSeed(seed_value);
 
-
+  // // // // // // // // // // // // // // // // // // // // // // /
 
   seed.close();
 
